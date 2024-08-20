c93bbd8e43174a753a948fed7bbf65c5c7dcc856
Junior Dario Rotela Fretes <juniorrotela@MacBook-Pro-de-Junior.local>
2024-08-20 07:12:02 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+import Work from "./queries/works";
+import Registration from "./registration";
+import Queries from "./queries";
+import Activity from "./registration/activity";
+import Owner from "./registration/owner";
+import OwnerQuery from "./queries/owners";
+import { Login } from "./login";
+import Inventario from "./queries/inventario/index";
+import useInactivityTimeout from "./customHooks/inactivityHook"; // Asegúrate de tener la ruta correcta
+import ProtectedRoute from "./components/ProtectedRoute"; // Asegúrate de tener la ruta correcta
+import Products from "./registration/Products";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+  const location = useLocation();
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const isLoginRoute = location.pathname === "/login";
+
+  useInactivityTimeout(1800000); // 30 minutos
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          {!isLoginRoute && !isAuthenticated && <Navigate to="/login" replace />}
+          {!isLoginRoute && isAuthenticated && <Sidebar isSidebar={isSidebar} />}
+          <main className="content">
+            {!isLoginRoute && isAuthenticated && <Topbar setIsSidebar={setIsSidebar} />}
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+              <Route path="/work" element={<ProtectedRoute><Work /></ProtectedRoute>} />
+              <Route path="/team" element={<ProtectedRoute><Team /></ProtectedRoute>} />
+              <Route path="/contacts" element={<ProtectedRoute><Contacts /></ProtectedRoute>} />
+              <Route path="/invoices" element={<ProtectedRoute><Invoices /></ProtectedRoute>} />
+              <Route path="/form" element={<ProtectedRoute><Form /></ProtectedRoute>} />
+              <Route path="/registration" element={<ProtectedRoute><Registration /></ProtectedRoute>} />
+              <Route path="/activity" element={<ProtectedRoute><Activity /></ProtectedRoute>} />
+              <Route path="/owner" element={<ProtectedRoute><Owner /></ProtectedRoute>} />
+              <Route path="/owner/query" element={<ProtectedRoute><OwnerQuery /></ProtectedRoute>} />
+              <Route path="/queries" element={<ProtectedRoute><Queries /></ProtectedRoute>} />
+              <Route path="/bar" element={<ProtectedRoute><Bar /></ProtectedRoute>} />
+              <Route path="/pie" element={<ProtectedRoute><Pie /></ProtectedRoute>} />
+              <Route path="/line" element={<ProtectedRoute><Line /></ProtectedRoute>} />
+              <Route path="/faq" element={<ProtectedRoute><FAQ /></ProtectedRoute>} />
+              <Route path="/calendar" element={<ProtectedRoute><Calendar /></ProtectedRoute>} />
+              <Route path="/geography" element={<ProtectedRoute><Geography /></ProtectedRoute>} />
+              <Route path="/Inventario" element={<ProtectedRoute><Inventario /></ProtectedRoute>} />
+              <Route path="/Products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/login/index.jsx
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import { Box, Button, TextField, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import Header from '../components/Header';
+import axios from 'axios'; // Necesitarás axios o fetch para las solicitudes HTTP
+import { PostLogin } from '../services/login.services';
+import ModalCharge from "../modal/modalCharge";
+import { useNavigate } from "react-router-dom";
+
+
+
+export const Login = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    // Estado para los inputs
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [level, setLevel] = useState(null);
+
+
+
+  const navigate = useNavigate();
+
+    // Función para manejar el cambio en los inputs
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        if (id === 'user') {
+            setUsername(value);
+        } else if (id === 'password') {
+            setPassword(value);
+        }
+    };
+
+    // Función para manejar el clic en el botón de inicio de sesión
+    const handleLogin = async () => {
+        setIsLoading(true);
+        const data = { username, password };
+    
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 3000));
+            const response = await PostLogin(data);
+    
+            console.log('Login successful:', response);
+            setLevel(response.level);
+            localStorage.setItem('authToken', response.token);
+            localStorage.setItem('userLevel', response.level); // Guarda el nivel del usuario
+    
+            setIsLoading(false);
+            navigate('/'); // Redirige a la página principal después del login
+        } catch (error) {
+            console.error('Error logging in:', error);
+            setIsLoading(false);
+        }
+    };
+
+//   console.log("level",level);
+
+
+    return (
+        <Box sx={{
+            background:"#1f2a40",
+            height:"550px",
+            width:"700px",
+            margin:"auto",
+            marginTop:"100px",
+            boxShadow:"0 0 5px 0px #000000",
+            borderRadius:"10px"
+        }}>
+            <Box sx={{
+                paddingTop:"10px",
+                paddingLeft:"50px",
+                fontSize:"20px",
+                display:"flex",
+                justifyContent:"space-between",
+                marginRight:"80px",
+                marginBottom:"-20px"
+            }}>
+                <h1>Iniciar Sesion</h1>
+                <img className='logo'
+                    style={{
+                        width:"250px",
+                        height:"100px",
+                        marginTop:"35px"
+                    }}
+                    src="../../assets/helios12.png" alt="logo" />
+            </Box>
+            <Box sx={{
+                paddingLeft:"50px",
+                fontSize:"20px",
+                display:"flex",
+                flexDirection:"column",
+
+                "& input": {
+                    height:"42px",
+                    width:"90%",
+                    border:"none",
+                    borderRadius:"8px",
+                    marginTop:"5px",
+                    marginBottom:"15px",
+                    fontSize:"18px"
+                },
+                "& input:hover": {
+                    border:"3px solid rgb(98 102 241)"
+                },
+            }}>
+                <label htmlFor="user">Nombre de Usuario</label>
+                <input
+                    className='input'
+                    id='user'
+                    type="text"
+                    placeholder='Ingrese su Usuario'
+                    value={username}
+                    onChange={handleChange}
+                />
+                <label htmlFor="password">Contraseña</label>
+                <input
+                    className='input'
+                    id='password'
+                    type="password"
+                    placeholder='Ingrese su Contraseña'
+                    value={password}
+                    onChange={handleChange}
+                />
+            </Box>
+            <Box sx={{
+                paddingLeft:"50px",
+                display:"flex",
+                justifyContent:"space-between",
+                marginRight:"80px",
+                fontSize:"25px",
+                marginTop:"20px",
+                "& button":{
+                    height:"50px",
+                    width:"180px",
+                    borderRadius:"8px",
+                    background:"rgb(98 102 241)",
+                    border:"none",
+                    color:"#ffffff",
+                    fontSize:"20px"
+                },
+                "& button:hover":{
+                    background:"#151632",
+                },
+
+                "& label":{
+                    fontSize:"22px",
+                    marginTop:"10px",
+                }
+            }}>
+                <Button
+                    className='button'
+                    onClick={handleLogin}
+                >
+                    Iniciar Sesion
+                </Button>
+                <label htmlFor="Registrarse" className='label'>Registrarse</label>
+            </Box>
+            <ModalCharge isLoading={isLoading} />
+        </Box>
+
+    )
+}
+
+
